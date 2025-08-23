7e3446e7795a46e00a2a973f49be6bb683364361
Abhay Sharma <abhaysharma.mrt@gmail.com>
2025-08-24 01:13:46 +0530

    Fixed bug that occurs when you try to access saved without logging in

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,22 +1,26 @@
-import { View, Text, Image, FlatList } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { icons } from '@/constants/icons'
 import MovieCard from '@/components/MovieCard'
+import { images } from '@/constants/images'
 import { useMovies } from '@/contexts/SavedMovieContext'
+import { useUser } from '@/contexts/UserContext'
 import { fetchMoviesDetails } from '@/services/api'
-import { useFocusEffect } from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native'
+import React, { useState } from 'react'
+import { FlatList, Image, Text, View } from 'react-native'
 
 const Saved = () => {
 
   const [savedMovies, setSavedMovies] = useState<MovieDetails[]>([]);
+  const user = useUser()
 
   const saveMovies = useMovies();
   const getMovies = async () => {
-    const data = await saveMovies.getSavedMovies();
-    if (data) {
-      for (const item of data) {
-        const itemdata = await fetchMoviesDetails(item.movieId);
-        setSavedMovies(prev => [...prev, itemdata]);
+    if (user?.current) {
+      const data = await saveMovies.getSavedMovies();
+      if (data) {
+        for (const item of data) {
+          const itemdata = await fetchMoviesDetails(item.movieId);
+          setSavedMovies(prev => [...prev, itemdata]);
+        }
       }
     }
   };
@@ -24,8 +28,10 @@ const Saved = () => {
 
   useFocusEffect(
     React.useCallback(() => {
-      setSavedMovies([])
-      getMovies();
+      if (user?.current) {
+        setSavedMovies([])
+        getMovies();
+      }
     }, [])
   );
 
@@ -34,20 +40,29 @@ const Saved = () => {
 
   return (
     <View className='bg-primary flex-1 px-6'>
-      <Text className='text-4xl text-white mt-[25%]'>Saved Movies</Text>
-      <FlatList
-        data={savedMovies}
-        renderItem={({ item }) => <MovieCard {...item} />}
-        keyExtractor={(item) => `${item.id}_${item.title}`}
-        numColumns={3}
-        columnWrapperStyle={{
-          justifyContent: "flex-start",
-          gap: 20,
-          paddingRight: 5,
-          marginBottom: 10,
-        }}
-        className="mt-10 pb-32"
-      />
+      <Image source={images.bg} className="absolute w-full z-0" />
+      {user?.current ?
+        (<>
+          <Text className='text-4xl text-white mt-[25%]'>Saved Movies</Text>
+          <FlatList
+            data={savedMovies}
+            renderItem={({ item }) => <MovieCard {...item} />}
+            keyExtractor={(item) => `${item.id}_${item.title}`}
+            numColumns={3}
+            columnWrapperStyle={{
+              justifyContent: "flex-start",
+              gap: 20,
+              paddingRight: 5,
+              marginBottom: 10,
+            }}
+            className="mt-10 pb-32"
+          />
+        </>) :
+        (<View className='w-full h-[70%] justify-center items-center'>
+          <Text className='text-3xl text-white'>Login to See Saved Movies</Text>
+        </View>)
+      }
+
     </View>
   )
 }
